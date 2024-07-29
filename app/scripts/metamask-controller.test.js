Tcrypto126/metamask-extension

commit 72d4b5265c39f28df0a896aaebfc1452745ee1b0

fix(deps): obj-multiplex@^1.0.0 -> @metamask/object-multiplex@^2.0.0 (#24533)

app/scripts/metamask-controller.js
@@ -277,7 +277,7 @@ import createTabIdMiddleware from './lib/createTabIdMiddleware';
 import { NetworkOrderController } from './controllers/network-order';
 import { AccountOrderController } from './controllers/account-order';
 import createOnboardingMiddleware from './lib/createOnboardingMiddleware';
-import { setupMultiplex } from './lib/stream-utils';
+import { isStreamWritable, setupMultiplex } from './lib/stream-utils';
 import PreferencesController from './controllers/preferences';
 import AppStateController from './controllers/app-state';
 import AlertController from './controllers/alert';
@@ -4890,7 +4890,7 @@ export default class MetamaskController extends EventEmitter {
       ),
     );
     const handleUpdate = (update) => {
-      if (outStream._writableState.ended) {
+      if (!isStreamWritable(outStream)) {
         return;
       }
       // send notification to client-side
@@ -4902,7 +4902,7 @@ export default class MetamaskController extends EventEmitter {
     };
     this.on('update', handleUpdate);
     const startUISync = () => {
-      if (outStream._writableState.ended) {
+      if (!isStreamWritable(outStream)) {
         return;
       }
       // send notification to client-side

app/scripts/contentscript.js
@@ -1,6 +1,6 @@
 import { WindowPostMessageStream } from '@metamask/post-message-stream';
 import PortStream from 'extension-port-stream';
-import ObjectMultiplex from 'obj-multiplex';
+import ObjectMultiplex from '@metamask/object-multiplex';
 import pump from 'pump';
 import { obj as createThoughStream } from 'through2';
 import browser from 'webextension-polyfill';
@@ -94,8 +94,8 @@ const setupPhishingExtStreams = () => {
       {
         target: PHISHING_WARNING_PAGE, // the post-message-stream "target"
         data: {
-          // this object gets passed to obj-multiplex
-          name: PHISHING_SAFELIST, // the obj-multiplex channel name
+          // this object gets passed to @metamask/object-multiplex
+          name: PHISHING_SAFELIST, // the @metamask/object-multiplex channel name
           data: {
             jsonrpc: '2.0',
             method: 'METAMASK_STREAM_FAILURE',
@@ -456,8 +456,8 @@ function extensionStreamMessageListener(msg) {
       {
         target: INPAGE, // the post-message-stream "target"
         data: {
-          // this object gets passed to obj-multiplex
-          name: PROVIDER, // the obj-multiplex channel name
+          // this object gets passed to @metamask/object-multiplex
+          name: PROVIDER, // the @metamask/object-multiplex channel name
           data: {
             jsonrpc: '2.0',
             method: 'METAMASK_EXTENSION_CONNECT_CAN_RETRY',
@@ -472,15 +472,15 @@ function extensionStreamMessageListener(msg) {
 /**
  * This function must ONLY be called in pump destruction/close callbacks.
  * Notifies the inpage context that streams have failed, via window.postMessage.
- * Relies on obj-multiplex and post-message-stream implementation details.
+ * Relies on @metamask/object-multiplex and post-message-stream implementation details.
  */
 function notifyInpageOfStreamFailure() {
   window.postMessage(
     {
       target: INPAGE, // the post-message-stream "target"
       data: {
-        // this object gets passed to obj-multiplex
-        name: PROVIDER, // the obj-multiplex channel name
+        // this object gets passed to @metamask/object-multiplex
+        name: PROVIDER, // the @metamask/object-multiplex channel name
         data: {
           jsonrpc: '2.0',
           method: 'METAMASK_STREAM_FAILURE',

app/scripts/lib/createMetaRPCHandler.js
@@ -1,9 +1,10 @@
 import { ethErrors, serializeError } from 'eth-rpc-errors';
 import { isManifestV3 } from '../../../shared/modules/mv3.utils';
+import { isStreamWritable } from './stream-utils';
 
 const createMetaRPCHandler = (api, outStream, store, localStoreApiWrapper) => {
   return async (data) => {
-    if (outStream._writableState.ended) {
+    if (!isStreamWritable(outStream)) {
       return;
     }
     if (!api[data.method]) {
@@ -29,7 +30,7 @@ const createMetaRPCHandler = (api, outStream, store, localStoreApiWrapper) => {
       }
     }
 
-    if (outStream._writableState.ended) {
+    if (!isStreamWritable(outStream)) {
       if (error) {
         console.error(error);
       }

app/scripts/lib/stream-utils.js
@@ -1,4 +1,4 @@
-import ObjectMultiplex from 'obj-multiplex';
+import ObjectMultiplex from '@metamask/object-multiplex';
 import pump from 'pump';
 
 import { EXTENSION_MESSAGES } from '../../../shared/constants/app';
@@ -23,4 +23,29 @@ export function setupMultiplex(connectionStream) {
     }
   });
   return mux;
+}
+
+/**
+ * Checks if a stream is writable and usable
+ *
+ * @param {stream.Stream} stream - the stream to check
+ * @returns {boolean} if the stream can be written to
+ */
+export function isStreamWritable(stream) {
+  /**
+   * Roughly:
+   *   stream.writable:
+   *     readable-stream-3 (confusingly: not mentioned in docs for streamsv2 and not consistently implemented there, despite v3 docs mentioning it as older)
+   *     readable-stream-4/NodeStream (here it's mentioned as introduced much later)
+   *   stream.destroyed:
+   *     readable-stream-4/NodeStream (docs mention it as introduced in v2 despite being absent from both implementation and docs of v2 and v3)
+   *   stream._writableState.ended:
+   *     Present in all implementations, seems like the most reasonable fallback for legacy.
+   *
+   * The only accurate references seem to be sources for Node.js and readable-stream. Intended compatibility must be ensured by tests.
+   */
+
+  return Boolean(
+    stream.writable && !stream.destroyed && !stream._writableState?.ended,
+  );
 }

commit 3a861639f045c12c308946cdf831fcc3be769f1b

fix: setupControllerConnection outstream end event listener (#26141)
## **Description**

This PR addresses a problem which had the following surface level
symptoms: when the UI is closed, the extension continues to send network
requests to the provider.

Upon some investigation, we discovered that these requests were the
result of polling initiated from the account tracker. And then upon
further investigation, we discovered that the callback in the
`outstream` `'end'` listener in `setupControllerConnection` of
`metamask-controller.js` was not being called when the UI was closed.

This problem was introduced with
https://github.com/MetaMask/metamask-extension/pull/24533. That PR
updates the `object-multiplex` dependency to v2.0.0. The major change in
that update is the update of the `readable-stream` dependency from a v2
version to a v3 version. With that update, we see new `premature-close`
errors in the background. It seems that these errors are still present
in v2, but they are handled differently, resulting in an `'error'` event
instead of an `'end'` event. We still have not established a definitive
understanding of why this is happening, and follow up investigation will
be needed.

However, this PR still puts forward a fix that safely resolves the
issue. The solution added here is to make use of the
`readableStream.finished` function. The callback passed to this function
will be called when the stream is ended. This function has the same
behaviour as the `outstream.on('end'` listener had prior to the
aforementioned PR.

Instead of fully replacing the existing listener, we have added the
`readableStream.finished` call in addition to the existing listener. The
reason for this is explained in a code comment:

> The presence of both of the below handlers may be redundant. After
upgrading metamask/object-multiples to v2.0.0, which included an upgrade
of readable-streams from v2 to v3, we saw that the `outStream.on('end'`

app/scripts/metamask-controller.js
@@ -1,5 +1,5 @@
 import EventEmitter from 'events';
-import { pipeline } from 'readable-stream';
+import { finished, pipeline } from 'readable-stream';
 import {
   AssetsContractController,
   CurrencyRateController,
@@ -5090,14 +5090,39 @@ export default class MetamaskController extends EventEmitter {
       this.once('startUISync', startUISync);
     }
 
-    outStream.on('end', () => {
-      this.activeControllerConnections -= 1;
-      this.emit(
-        'controllerConnectionChanged',
-        this.activeControllerConnections,
-      );
-      this.removeListener('update', handleUpdate);
-    });
+    const outstreamEndHandler = () => {
+      if (!outStream.mmFinished) {
+        this.activeControllerConnections -= 1;
+        this.emit(
+          'controllerConnectionChanged',
+          this.activeControllerConnections,
+        );
+        outStream.mmFinished = true;
+        this.removeListener('update', handleUpdate);
+      }
+    };
+
+    // The presence of both of the below handlers may be redundant.
+    // After upgrading metamask/object-multiples to v2.0.0, which included
+    // an upgrade of readable-streams from v2 to v3, we saw that the
+    // `outStream.on('end'` handler was almost never being called. This seems to
+    // related to how v3 handles errors vs how v2 handles errors; there
+    // are "premature close" errors in both cases, although in the case
+    // of v2 they don't prevent `outStream.on('end'` from being called.
+    // At the time that this comment was committed, it was known that we
+    // need to investigate and resolve the underlying error, however,
+    // for expediency, we are not addressing them at this time. Instead, we
+    // can observe that `readableStream.finished` preserves the same
+    // functionality as we had when we relied on readable-stream v2. Meanwhile,
+    // the `outStream.on('end')` handler was observed to have been called at least once.
+    // In an abundance of caution to prevent against unexpected future behavioral changes in
+    // streams implementations, we redundantly use multiple paths to attach the same event handler.
+    // The outstreamEndHandler therefore needs to be idempotent, which introduces the `mmFinished` property.
+
+    outStream.mmFinished = false;
+    finished(outStream, outstreamEndHandler);
+    outStream.once('close', outstreamEndHandler);
+    outStream.once('end', outstreamEndHandler);
   }
 
   /**

app/scripts/metamask-controller.test.js
@@ -27,6 +27,7 @@ import {
   RatesController,
   TokenListController,
 } from '@metamask/assets-controllers';
+import ObjectMultiplex from '@metamask/object-multiplex';
 import { TrezorKeyring } from '@metamask/eth-trezor-keyring';
 import { LedgerKeyring } from '@metamask/eth-ledger-bridge-keyring';
 import { NETWORK_TYPES } from '../../shared/constants/network';
@@ -1406,8 +1407,287 @@ describe('MetaMaskController', () => {
         });
 
         metamaskController.setupTrustedCommunication(streamTest, messageSender);
+
         await promise;
         streamTest.end();
+      });
+
+      it('uses a new multiplex to set up a connection', () => {
+        jest.spyOn(metamaskController, 'setupControllerConnection');
+
+        const streamTest = createThroughStream((chunk, _, cb) => {
+          cb(chunk);
+        });
+
+        metamaskController.setupTrustedCommunication(streamTest, {});
+
+        expect(metamaskController.setupControllerConnection).toHaveBeenCalled();
+        expect(
+          metamaskController.setupControllerConnection,
+        ).toHaveBeenCalledWith(
+          expect.objectContaining({
+            _name: 'controller',
+            _parent: expect.any(ObjectMultiplex),
+          }),
+        );
+      });
+
+      const createTestStream = () => {
+        const {
+          promise: onFinishedCallbackPromise,
+          resolve: onFinishedCallbackResolve,
+        } = deferredPromise();
+        const { promise: onStreamEndPromise, resolve: onStreamEndResolve } =
+          deferredPromise();
+        const testStream = createThroughStream((chunk, _, cb) => {
+          expect(chunk.name).toStrictEqual('controller');
+          onStreamEndResolve();
+          cb();
+        });
+
+        return {
+          onFinishedCallbackPromise,
+          onStreamEndPromise,
+          onFinishedCallbackResolve,
+          testStream,
+        };
+      };
+
+      it('sets up a controller connection which emits a controllerConnectionChanged event when the controller connection is created and ended, and activeControllerConnections are updated accordingly', async () => {
+        const mockControllerConnectionChangedHandler = jest.fn();
+
+        const {
+          onStreamEndPromise,
+          onFinishedCallbackPromise,
+          onFinishedCallbackResolve,
+          testStream,
+        } = createTestStream();
+
+        metamaskController.on(
+          'controllerConnectionChanged',
+          (activeControllerConnections) => {
+            mockControllerConnectionChangedHandler(activeControllerConnections);
+            if (
+              mockControllerConnectionChangedHandler.mock.calls.length === 2
+            ) {
+              onFinishedCallbackResolve();
+            }
+          },
+        );
+
+        expect(metamaskController.activeControllerConnections).toBe(0);
+
+        metamaskController.setupTrustedCommunication(testStream, {});
+
+        expect(mockControllerConnectionChangedHandler).toHaveBeenCalledTimes(1);
+        expect(mockControllerConnectionChangedHandler).toHaveBeenLastCalledWith(
+          1,
+        );
+
+        expect(metamaskController.activeControllerConnections).toBe(1);
+
+        await onStreamEndPromise;
+        testStream.end();
+
+        await onFinishedCallbackPromise;
+
+        expect(metamaskController.activeControllerConnections).toBe(0);
+        expect(mockControllerConnectionChangedHandler).toHaveBeenCalledTimes(2);
+        expect(mockControllerConnectionChangedHandler).toHaveBeenLastCalledWith(
+          0,
+        );
+      });
+
+      it('can be called multiple times to set up multiple controller connections, which can be ended independently', async () => {
+        const mockControllerConnectionChangedHandler = jest.fn();
+
+        const testStreams = [
+          createTestStream(),
+          createTestStream(),
+          createTestStream(),
+          createTestStream(),
+          createTestStream(),
+        ];
+        metamaskController.on(
+          'controllerConnectionChanged',
+          (activeControllerConnections) => {
+            const initialChangeHandlerCallCount =
+              mockControllerConnectionChangedHandler.mock.calls.length;
+            mockControllerConnectionChangedHandler(activeControllerConnections);
+
+            if (
+              initialChangeHandlerCallCount === 5 &&
+              activeControllerConnections === 4
+            ) {
+              testStreams[1].onFinishedCallbackResolve();
+            }
+            if (
+              initialChangeHandlerCallCount === 7 &&
+              activeControllerConnections === 2
+            ) {
+              testStreams[3].onFinishedCallbackResolve();
+              testStreams[4].onFinishedCallbackResolve();
+            }
+            if (
+              initialChangeHandlerCallCount === 9 &&
+              activeControllerConnections === 0
+            ) {
+              testStreams[2].onFinishedCallbackResolve();
+              testStreams[0].onFinishedCallbackResolve();
+            }
+          },
+        );
+
+        metamaskController.setupTrustedCommunication(
+          testStreams[0].testStream,
+          {},
+        );
+        metamaskController.setupTrustedCommunication(
+          testStreams[1].testStream,
+          {},
+        );
+        metamaskController.setupTrustedCommunication(
+          testStreams[2].testStream,
+          {},
+        );
+        metamaskController.setupTrustedCommunication(
+          testStreams[3].testStream,
+          {},
+        );
+        metamaskController.setupTrustedCommunication(
+          testStreams[4].testStream,
+          {},
+        );
+
+        expect(metamaskController.activeControllerConnections).toBe(5);
+
+        await testStreams[1].promise;
+        testStreams[1].testStream.end();
+
+        await testStreams[1].onFinishedCallbackPromise;
+
+        expect(metamaskController.activeControllerConnections).toBe(4);
+
+        await testStreams[3].promise;
+        testStreams[3].testStream.end();
+
+        await testStreams[4].promise;
+        testStreams[4].testStream.end();
+
+        await testStreams[3].onFinishedCallbackPromise;
+        await testStreams[4].onFinishedCallbackPromise;
+
+        expect(metamaskController.activeControllerConnections).toBe(2);
+
+        await testStreams[2].promise;
+        testStreams[2].testStream.end();
+
+        await testStreams[0].promise;
+        testStreams[0].testStream.end();
+
+        await testStreams[2].onFinishedCallbackPromise;
+        await testStreams[0].onFinishedCallbackPromise;
+
+        expect(metamaskController.activeControllerConnections).toBe(0);
+      });
+
+      // this test could be improved by testing for actual behavior of handlers,
+      // without touching rawListeners from test
+      it('attaches listeners for trusted communication streams and removes them as streams close', async () => {
+        jest
+          .spyOn(metamaskController, 'triggerNetworkrequests')
+          .mockImplementation();
+        jest
+          .spyOn(metamaskController.onboardingController.store, 'getState')
+          .mockReturnValue({ completedOnboarding: true });
+        const mockControllerConnectionChangedHandler = jest.fn();
+
+        const testStreams = [
+          createTestStream(),
+          createTestStream(2),
+          createTestStream(3),
+          createTestStream(4),
+          createTestStream(5),
+        ];
+        const baseUpdateListenerCount =
+          metamaskController.rawListeners('update').length;
+
+        metamaskController.on(
+          'controllerConnectionChanged',
+          (activeControllerConnections) => {
+            const initialChangeHandlerCallCount =
+              mockControllerConnectionChangedHandler.mock.calls.length;
+            mockControllerConnectionChangedHandler(activeControllerConnections);
+            if (
+              initialChangeHandlerCallCount === 8 &&
+              activeControllerConnections === 1
+            ) {
+              testStreams[1].onFinishedCallbackResolve();
+              testStreams[3].onFinishedCallbackResolve();
+              testStreams[4].onFinishedCallbackResolve();
+              testStreams[2].onFinishedCallbackResolve();
+            }
+            if (
+              initialChangeHandlerCallCount === 9 &&
+              activeControllerConnections === 0
+            ) {
+              testStreams[0].onFinishedCallbackResolve();
+            }
+          },
+        );
+
+        metamaskController.setupTrustedCommunication(
+          testStreams[0].testStream,
+          {},
+        );
+        metamaskController.setupTrustedCommunication(
+          testStreams[1].testStream,
+          {},
+        );
+        metamaskController.setupTrustedCommunication(
+          testStreams[2].testStream,
+          {},
+        );
+        metamaskController.setupTrustedCommunication(
+          testStreams[3].testStream,
+          {},
+        );
+        metamaskController.setupTrustedCommunication(
+          testStreams[4].testStream,
+          {},
+        );
+
+        await testStreams[1].promise;
+
+        expect(metamaskController.rawListeners('update')).toHaveLength(
+          baseUpdateListenerCount + 5,
+        );
+
+        testStreams[1].testStream.end();
+        await testStreams[3].promise;
+        testStreams[3].testStream.end();
+        testStreams[3].testStream.end();
+
+        await testStreams[4].promise;
+        testStreams[4].testStream.end();
+        await testStreams[2].promise;
+        testStreams[2].testStream.end();
+        await testStreams[1].onFinishedCallbackPromise;
+        await testStreams[3].onFinishedCallbackPromise;
+        await testStreams[4].onFinishedCallbackPromise;
+        await testStreams[2].onFinishedCallbackPromise;
+        expect(metamaskController.rawListeners('update')).toHaveLength(
+          baseUpdateListenerCount + 1,
+        );
+
+        await testStreams[0].promise;
+        testStreams[0].testStream.end();
+
+        await testStreams[0].onFinishedCallbackPromise;
+
+        expect(metamaskController.rawListeners('update')).toHaveLength(
+          baseUpdateListenerCount,
+        );
       });
     });
 

test/e2e/tests/api-usage/account-tracker-api-usage.spec.ts
@@ -1,0 +1,209 @@
+import { strict as assert } from 'assert';
+import { MockedEndpoint } from 'mockttp';
+import { JsonRpcRequest } from '@metamask/utils';
+import {
+  withFixtures,
+  defaultGanacheOptions,
+  unlockWallet,
+  veryLargeDelayMs,
+} from '../../helpers';
+import FixtureBuilder from '../../fixture-builder';
+import { Driver } from '../../webdriver/driver';
+import { Mockttp } from '../../mock-e2e';
+
+async function mockInfura(mockServer: Mockttp): Promise<MockedEndpoint[]> {
+  const blockNumber = { value: 0 };
+
+  return [
+    await mockServer
+      .forPost(/infura/u)
+      .withJsonBodyIncluding({ method: 'eth_blockNumber' })
+      .times(50)
+      .thenCallback(() => {
+        // We need to ensure the mocked block number keeps increasing,
+        // as this results in block tracker listeners firing, which is
+        // needed for the potential account tracker network requests being
+        // tested against here.
+        blockNumber.value += 1;
+
+        return {
+          statusCode: 200,
+          json: {
+            jsonrpc: '2.0',
+            id: '1111111111111111',
+            result: blockNumber.value.toString(16),
+          },
+        };
+      }),
+    await mockServer.forPost(/infura/u).thenCallback(() => {
+      return {
+        statusCode: 200,
+        json: {
+          jsonrpc: '2.0',
+          id: '1111111111111111',
+          result: '0x1',
+        },
+      };
+    }),
+    await mockServer.forGet(/infura/u).thenCallback(() => {
+      return {
+        statusCode: 200,
+        json: {
+          jsonrpc: '2.0',
+          id: '1111111111111111',
+          result: '0x0',
+        },
+      };
+    }),
+  ];
+}
+
+async function getAllInfuraJsonRpcRequests(
+  mockedEndpoint: MockedEndpoint[],
+): Promise<JsonRpcRequest[]> {
+  const allInfuraJsonRpcRequests: JsonRpcRequest[] = [];
+  let seenRequests;
+  let seenProviderRequests;
+
+  for (const m of mockedEndpoint) {
+    seenRequests = await m.getSeenRequests();
+    seenProviderRequests = seenRequests.filter((request) =>
+      request.url.match('infura'),
+    );
+
+    for (const r of seenProviderRequests) {
+      const json = await r.body.getJson();
+      if (json !== undefined) {
+        allInfuraJsonRpcRequests.push(json);
+      }
+    }
+  }
+
+  return allInfuraJsonRpcRequests;
+}
+
+function getSpecifiedJsonRpcRequests(
+  jsonRpcRequestArray: JsonRpcRequest[],
+  methodsToGet: string[],
+) {
+  return jsonRpcRequestArray.filter(({ method }) =>
+    methodsToGet.includes(method),
+  );
+}
+
+describe('Account Tracker API Usage', function () {
+  it('should not make eth_call or eth_getBalance requests before the UI is opened and should make those requests after the UI is opened', async function () {
+    // Note: we are not testing that eth_getBlockByNumber is not called before the UI
+    // is opened because there is a known bug that results in it being called if the
+    // user is already onboarded: https://github.com/MetaMask/MetaMask-planning/issues/2151
+    // Once that issue is resolved, we can add eth_getBlockByNumber to the below array.
+    const RPC_METHODS_TO_TEST = ['eth_call', 'eth_getBalance'];
+
+    await withFixtures(
+      {
+        fixtures: new FixtureBuilder().withNetworkControllerOnMainnet().build(),
+        ganacheOptions: defaultGanacheOptions,
+        title: this.test?.fullTitle(),
+        testSpecificMock: mockInfura,
+      },
+      async ({
+        driver,
+        mockedEndpoint,
+      }: {
+        driver: Driver;
+        mockedEndpoint: MockedEndpoint[];
+      }) => {
+        await driver.delay(veryLargeDelayMs);
+        let allInfuraJsonRpcRequests = await getAllInfuraJsonRpcRequests(
+          mockedEndpoint,
+        );
+        let rpcMethodsToTestRequests = getSpecifiedJsonRpcRequests(
+          allInfuraJsonRpcRequests,
+          RPC_METHODS_TO_TEST,
+        );
+
+        assert.ok(
+          rpcMethodsToTestRequests.length === 0,
+          `An ${RPC_METHODS_TO_TEST.join(
+            ' or ',
+          )} request has been made to infura before opening the UI`,
+        );
+
+        await unlockWallet(driver);
+        await driver.delay(veryLargeDelayMs);
+
+        allInfuraJsonRpcRequests = await getAllInfuraJsonRpcRequests(
+          mockedEndpoint,
+        );
+        rpcMethodsToTestRequests = getSpecifiedJsonRpcRequests(
+          allInfuraJsonRpcRequests,
+          RPC_METHODS_TO_TEST,
+        );
+
+        assert.ok(
+          rpcMethodsToTestRequests.length > 0,
+          `No ${RPC_METHODS_TO_TEST.join(
+            ' or ',
+          )} request has been made to infura since opening the UI`,
+        );
+      },
+    );
+  });
+
+  it('should not make eth_call or eth_getBalance or eth_getBlockByNumber requests after the UI is closed', async function () {
+    const RPC_METHODS_TO_TEST = [
+      'eth_getBlockByNumber',
+      'eth_call',
+      'eth_getBalance',
+    ];
+
+    await withFixtures(
+      {
+        fixtures: new FixtureBuilder().withNetworkControllerOnMainnet().build(),
+        ganacheOptions: defaultGanacheOptions,
+        title: this.test?.fullTitle(),
+        testSpecificMock: mockInfura,
+      },
+      async ({
+        driver,
+        mockedEndpoint,
+      }: {
+        driver: Driver;
+        mockedEndpoint: MockedEndpoint[];
+      }) => {
+        await unlockWallet(driver);
+        await driver.delay(veryLargeDelayMs);
+        const initialInfuraJsonRpcRequests = await getAllInfuraJsonRpcRequests(
+          mockedEndpoint,
+        );
+
+        await driver.openNewURL('about:blank');
+        // The delay is intentionally 20000, to ensure we cover at least 1 polling
+        // loop of time for the block tracker.
+        await driver.delay(20000);
+
+        const currentInfuraJsonRpcRequests = await getAllInfuraJsonRpcRequests(
+          mockedEndpoint,
+        );
+
+        const initialRpcMethodsToTestRequests = getSpecifiedJsonRpcRequests(
+          initialInfuraJsonRpcRequests,
+          RPC_METHODS_TO_TEST,
+        );
+
+        const currentRpcMethodsToTestRequests = getSpecifiedJsonRpcRequests(
+          currentInfuraJsonRpcRequests,
+          RPC_METHODS_TO_TEST,
+        );
+
+        assert.ok(
+          initialRpcMethodsToTestRequests.length ===
+            currentRpcMethodsToTestRequests.length,
+          `An ${RPC_METHODS_TO_TEST.join(
+            ' or ',
+          )} request has been made to infura after closing the UI`,
+        );
+      },
+    );
+  });
+});